c4f56d552b59fdf5efa6db593fa4d7a3ad9e4f04
seokyHan <sseok1979@naver.com>
2025-02-07 03:27:04 +0900

    chore : 기존 RDB 대기열 토큰 관련 로직 제거

--- src/main/java/com/server/concert_reservation/infrastructure/queue_token/entity/types/QueueTokenStatus.java
+++
@@ -1,5 +0,0 @@
-package com.server.concert_reservation.infrastructure.queue_token.entity.types;
-
-public enum QueueTokenStatus {
-    WAITING, ACTIVE, EXPIRED
-}
--- src/main/java/com/server/concert_reservation/interfaces/web/queue_token/dto/QueueTokenHttpRequest.java
+++
@@ -1,9 +0,0 @@
-package com.server.concert_reservation.interfaces.web.queue_token.dto;
-
-public class QueueTokenHttpRequest {
-
-    public record QueueTokenRequest(Long userId,
-                                    String token
-    ) {
-    }
-}
