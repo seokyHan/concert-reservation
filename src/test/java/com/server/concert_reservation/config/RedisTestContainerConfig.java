b56779e4f0589d89137514e09db487ae45b26b47
seokyHan <sseok1979@naver.com>
2025-02-19 11:13:00 +0900

    refactor : redis TestContainer 변수 분리

--- src/test/java/com/server/concert_reservation/config/RedisTestContainerConfig.java
+++ src/test/java/com/server/concert_reservation/config/RedisTestContainerConfig.java
@@ -1,15 +1,20 @@
 package com.server.concert_reservation.config;
 
+import jakarta.annotation.PreDestroy;
 import org.springframework.context.annotation.Configuration;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 @Configuration
 public class RedisTestContainerConfig {
 
+    @Container
+    static GenericContainer<?> REDIS_CONTAINER;
+
     static {
-        GenericContainer<?> REDIS_CONTAINER = new GenericContainer<>("redis:7.4.2")
+        REDIS_CONTAINER = new GenericContainer<>("redis:latest")
                 .withExposedPorts(6379)
                 .withReuse(true);
 
@@ -17,6 +22,13 @@ public class RedisTestContainerConfig {
 
         System.setProperty("spring.data.redis.host", REDIS_CONTAINER.getHost());
         System.setProperty("spring.data.redis.port", REDIS_CONTAINER.getMappedPort(6379).toString());
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+        if (REDIS_CONTAINER.isRunning()) {
+            REDIS_CONTAINER.stop();
+        }
     }
 
 }
